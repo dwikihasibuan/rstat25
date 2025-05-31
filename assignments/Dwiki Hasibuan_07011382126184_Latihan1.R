cd21ebeec74ee52a91c72a46759aa3b3495dd5f6
dwikihasibuan <dwikihasibuan@gmail.com>
2025-05-31 10:35:16 +0700

    Menambahkan jawaban untuk soal Latihan 1

---
+++ assignments/Dwiki Hasibuan_07011382126184_Latihan1.R
@@ -0,0 +1,45 @@
+#LATIHAN BAGIAN 1: OPERASI ARITMATIKA DASAR
+
+#1.) Buat variabel `a` dengan nilai 45 dan `b` dengan nilai 15. Hitung hasil dari `a + b` dan simpan dalam `total`.
+a <- 45
+b <- 15
+total <- a+b
+print(total)
+
+#2.) Hitung hasil dari `a - b` dan simpan dalam `selisih`.
+selisih <- a-b
+print(selisih)
+
+#3.) Buat variabel `c` dengan nilai 12. Hitung hasil `a * c / b` dan simpan dalam `hasil`.
+c <- 12
+hasil <- a*c/b
+print(hasil)
+
+#4.) Gunakan operator `%%` untuk mendapatkan sisa pembagian `a` dengan `b` dan simpan dalam `modulo`.
+modulo <- a %% b
+print(modulo)
+
+#5.) Hitung nilai absolut dari `-23` menggunakan fungsi `abs()` dan simpan dalam variabel `nilai_abs`.
+nilai_abs <- abs(-23)
+print(nilai_abs)
+
+#6.) Gunakan fungsi `sqrt()` untuk menghitung akar kuadrat dari 144.
+akar_kuadrat <- sqrt(144)
+print(akar_kuadrat)
+
+#7.) Buat variabel `x` bernilai 5. Hitung hasil dari `x^3`.
+x <- 5
+p.tiga <- x^3
+print(p.tiga)
+
+#8.) Hitung pembulatan ke atas dari 12.3 menggunakan `ceiling()`.
+p.ats <- ceiling(12.3)
+print(p.ats)
+
+#9.) Hitung pembulatan ke bawah dari 12.7 menggunakan `floor()`.
+p.bwh <- floor(12.7)
+print(p.bwh)
+
+#10.) Gunakan fungsi `round()` untuk membulatkan angka 17.567 ke dua angka desimal.
+bulat <- round(17.567, 2)
+print(bulat)
